e0ba995534e17f98b614717b72ffc1b33d9dd96c
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2013-12-30 19:59:02 +0100

    gpl

--- src/SFCSnumeric.pyx
+++ src/SFCSnumeric.pyx
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-""" Binning Data for Multiple Tau Algorithm
-    Paul Müller, Biotec - TU Dresden
+""" 
+    PyScanFCS
+    
+    Binning Data for Multiple Tau Algorithm
 
     As fast as you can get with python, binning the photon arrival times
     created by Photon.exe from correlator.com.
@@ -8,6 +10,21 @@
     See at the end of the file, which .dat file will be opened.
     In console, we ask for binning time in µs and an .int file
     is created.
+    
+    (C) 2012 Paul Müller
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License 
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 #import codecs, sys, win32console
--- src/edclasses.py
+++ src/edclasses.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
-""" SFCS
-    Paul Müller, Biotec - TU Dresden
+"""
+    PyScanFCS
 
-    EditedClasses
+    Module edclasses
     Contains classes that we edited.
-    Should make our classes more useful.
+    Makes these classes more useful for us.
 
+    (C) 2012 Paul Müller
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License 
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 import numpy as np
 from wx.lib.agw import floatspin        # Float numbers in spin fields
