6af5ad7640d4f9fb27fb703c51360abcd37e576c
paulmueller <paul.mueller@biotec.tu-dresden.de>
2015-01-03 18:54:37 +0100

    Windows build machine to Win7

---
+++ freeze_pyinstaller/win7_32bit_bundle_script.bat
@@ -0,0 +1,6 @@
+cd %~dp0
+
+cd ..
+
+
+pyinstaller -y .\freeze_pyinstaller\PyScanFCS_win7.spec
--- freeze_pyinstaller/windowsXP_32bit_bundle_script.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Python27\pyinstaller*
-del /q /s ..\PyScanFCS\pyinstaller-howto\build
-del /q /s ..\PyScanFCS\pyinstaller-howto\dist
-python pyinstaller.py ..\PyScanFCS\freeze_pyinstaller\PyScanFCS_win.spec
